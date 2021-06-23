91a5777ae95a0fa7804f727d04e7a3008b9b29eb
Hyunsulock <hateibs1@gmail.com>
2021-06-23 17:13:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import React, {useState, useEffect} from 'react';
+import VideoList from './video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+  fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=kr&key=AIzaSyDij5uthJaDftvlHUTuNPS6DKBARn9IbY0", requestOptions)
+    .then(response => response.json())
+    .then(result => setVideos(result.items))
+    .catch(error => console.log('error', error));
+    return () => {
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VideoList videos={videos}></VideoList>
   );
 }
 
